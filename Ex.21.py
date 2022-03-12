195e507aa5807e10ea261d587d776c6dd4ab1273
E-rickP <erick.pedrosa.b@gmail.com>
2022-03-12 17:14:39 -0300

    Update

--- Ex.21.py
+++ Ex.21.py
@@ -1,7 +1,7 @@
-from time import sleep
-import pygame
+from time import sleep;
+import pygame;
 pygame.init()
 
-pygame.mixer.music.load('Trem Das Onze(MP3_320K).mp3')
+pygame.mixer.music.load('20 Ligaçoes(MP3_320K).mp3')
 pygame.mixer.music.play()
 sleep(180)
