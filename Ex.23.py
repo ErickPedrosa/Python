8c1d74681728b9ac95d4baed91c70a14bd933055
E-rickP <erick.pedrosa.b@gmail.com>
2022-03-13 15:33:44 -0300

    update atividades aula 9

--- Ex.23.py
+++ Ex.23.py
@@ -1,11 +1,9 @@
-num = input("Digite um número entre 0 e 9999: ")
+num = int(input("Digite um número entre 0 e 9999: "))
 
-num = num.strip()
-
-num_m = num[0]
-num_c = num[1]
-num_d = num[2]
-num_u = num[3]
+num_m = num // 1000 % 10
+num_c = num // 100 % 10
+num_d = num // 10 % 10
+num_u = num // 1 % 10
 
 print("""
 unidades: {}
