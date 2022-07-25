70efcd37696217b41b950f35b04c303adc4a6dcd
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2022-07-25 15:40:36 -0300

    update

---
+++ Alura/OO/conta.py
@@ -0,0 +1,18 @@
+
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print(f"Construindo o objeto ... {self}")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        
+    def extrato(self):
+        print(f"Saldo de {self.saldo} do titular {self.titular}")
+
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        self.saldo -= valor
+        
