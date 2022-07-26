011cf7a1804b97a9e2b8003551c8c25d45471643
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2022-07-25 23:07:59 -0300

    update

--- Alura/OO/conta.py
+++ Alura/OO/conta.py
@@ -6,15 +6,23 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo_banco = "001"
         
     def extrato(self):
         print(f"Saldo de {self.__saldo} do titular {self.__titular}")
 
     def depositar(self, valor):
         self.__saldo += valor
+        
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return (valor_a_sacar <= valor_disponivel)
 
     def sacar(self, valor):
-        self.__saldo -= valor
+        if (self.__pode_sacar()):
+            self.__saldo -= valor
+        else:
+            print(f"O valor {valor} passou do limite")
         
     def transferir(self, destino, valor):
         self.sacar(valor)
@@ -36,6 +44,12 @@ class Conta:
     def set_limite(self, limite):
         self.__limite = limite
     
+    @staticmethod
+    def codigo_banco():
+        return "001"
     
+    @staticmethod
+    def codigos_bancos():
+        return {"BB":"001", "Caixa":"104", "Bradesco":"237"}
     
         
